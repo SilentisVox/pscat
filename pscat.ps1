6f46491bf324f204944ccdd526350a8d0b6335b2
SilentisVox <pdnvzjivvmum@gmail.com>
2025-06-04 19:14:47 -0400

    + Connect & listen methods

--- pscat.ps1
+++ pscat.ps1
@@ -27,4 +27,43 @@ class pscat
         }
         $this.Streams                   = @()
     }
+
+    [Bool] Start_Connect()
+    {
+        Write-Verbose "connecting [$($this.Address)] $($this.Port) ..."
+        $TcpClient                      = [Net.Sockets.TcpClient]::new($this.Address, $this.Port)
+
+        if (-not $TcpClient)
+        {
+            return $false
+        }
+
+        $this.Objects.TcpClient         = $TcpClient
+
+        return $true
+    }
+
+    [Bool] Start_Listen()
+    {
+        Write-Verbose "listening on [$($this.Address)] $($this.Port) ..."
+        $TcpListener                    = [Net.Sockets.TcpListener]::new($this.Address, $this.Port)
+        $TcpListener.Start()
+        $TcpClient                      = $TcpListener.AcceptTcpClient()
+
+        $LocalConnectionAddress         = $TcpClient.Client.LocalEndPoint.Address
+        $RemoteConnectionAddress        = $TcpClient.Client.RemoteEndPoint.Address
+        $RemoteConnectionPort           = $TcpClient.Client.RemoteEndPoint.Port
+
+        Write-Verbose "connect to [$LocalConnectionAddress] from [$RemoteConnectionAddress] $RemoteConnectionPort"
+
+        if (-not $TcpClient)
+        {
+            return $false
+        }
+
+        $TcpListener.Stop()
+        $this.Objects.TcpClient         = $TcpClient
+
+        return $true
+    }
 }
